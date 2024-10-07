6f86f3a27808ad86dc2326c9bf1ef73c03001c5d
ZzzwaHoon <kkjw94@gmail.com>
2024-10-07 17:14:36 +0900

    Java 프로그래밍 - 조건문 (if, switch)

--- Practice/Java_04/src/Main.java
+++ Practice/Java_04/src/Main.java
@@ -5,16 +5,53 @@ public class Main {
 
 //      1. 조건문 - if
         System.out.println("== if ==");
+        int waterTemperature = 99;
+        if (waterTemperature >= 100) {
+            System.out.println("물이 끓습니다.");
+        } else {
+            System.out.println("물을 끓이는 중입니다.");
+        }
 
+        int score = 90;
+        char grade = 0;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("grade = " + grade);
 
 
 //      2. 조건문 - switch
         System.out.println("== switch ==");
+        String fruit = "apple";
+
+        switch (fruit) {
+            case "apple" :
+                System.out.println(fruit + "은 5000원 입니다.");
+                break;
+            case "blueberry" :
+                System.out.println(fruit + "은 10000원 입니다.");
+                break;
+            default :
+                System.out.println("해당 과일이 없습니다.");
+                break;
+        }
 
 
 
 //      Q1. number의 값이 홀수인지 짝수인지 판단하는 코드를 작성하세요.
         int number = 5;
+        if (number % 2 == 0) {
+            System.out.println("짝수 입니다.");
+        } else {
+            System.out.println("홀수 입니다.");
+        }
 
 
 
@@ -33,6 +70,25 @@ public class Main {
 //        }
 //        System.out.println("grade = " + grade);
 
+        score = 90;
+        grade = 0;
+
+        switch (score / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            default:
+                grade = 'F';
+                break;
+        }
+        System.out.println("grade = " + grade);
 
     }
 }
