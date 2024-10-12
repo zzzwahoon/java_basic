afedf279f0c1d8edbef081af575f680f95b7a70f
ZzzwaHoon <kkjw94@gmail.com>
2024-10-12 15:44:48 +0900

    Java 프로그래밍 - 추상 클래스

--- Practice/Java_10/src/Practice.java
+++ Practice/Java_10/src/Practice.java
@@ -13,17 +13,72 @@ abstract class Device {
 }
 
 // UsbPort1 클래스
+class UsbPort1 extends Device {
+    UsbPort1(int id) {
+        this.deviceId = id;
+    }
+    
+    void deviceInfo() {
+        System.out.println("id = " + this.deviceId);
+    }
+
+    void connect() {
+        System.out.println("연결 하였습니다.");
+    }
+
+    void disconnect() {
+        System.out.println("연결이 해제되었습니다");
+    }
+
+    void send() {
+        System.out.println("데이터를 전송합니다. ");
+    }
+
+    void receive() {
+        System.out.println("데이터를 수신합니다");
+    }
+}
 
 
 // WiFi 클래스
+class WiFi extends Device {
+    public WiFi(int id) {
+        this.deviceId = id;
+    }
+
+    @Override
+    void deviceInfo() {
+
+    }
+
+    @Override
+    void connect() {
+
+    }
+
+    @Override
+    void disconnect() {
+
+    }
+
+    @Override
+    void send() {
+
+    }
+
+    @Override
+    void receive() {
+
+    }
+}
 
 
 public class Practice {
 
     public static void main(String[] args) {
         // Test code
-//        UsbPort1 usb1 = new UsbPort1(1);
-//        WiFi wifi = new WiFi(0);
+        UsbPort1 usb1 = new UsbPort1(1);
+        WiFi wifi = new WiFi(0);
     }
 
 }
